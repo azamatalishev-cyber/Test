691c6c0f8412e29a4e5b693ee1df41fb0c80814f
Igor Veyner <igorveyner95@gmail.com>
2021-07-17 18:35:20 -0400

    Update Test

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -14,7 +14,7 @@ const AboutMe = () => {
         <div className="about-text-container">
           <h2 className={`about-title ${aboutTitle}`}>Hello!</h2>
           <p className={`about-text ${aboutText}`}>
-            My name is Azamat.
+            My name is Not-Azamat.
           </p>
           <p className={`about-text ${aboutText}`}>
             I'm a NYC based Software Engineer and 
