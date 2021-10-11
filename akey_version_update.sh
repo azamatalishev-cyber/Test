22b0d0494bd95ca1d73e7cdd6b3c7dfde8cd6128
Azamat Alishev <azamat.alishev@stash.com>
2021-10-11 12:31:13 -0400

    test

--- akey_version_update.sh
+++ akey_version_update.sh
@@ -7,7 +7,7 @@ current_version=$(grep -oP "(\d+\.+\d+\d+\.+\d)?(\d+\.+\d+\.+\d)" $manifest_path
 latest_tag_version=$(curl https://hub.docker.com/v2/repositories/akeyless/base/tags | grep -oP "(\d+\.+\d+\.+\d\-+)|(\d+\.+\d+\.+\d)" | grep '[^-]$' | head -n 1)
 
 current_tag_version=$(grep -oP "(\d+\.+\d+\d+\.+\d)?(\d+\.+\d+\.+\d)" $manifest_path | head -n 1)
-
+echo $current_tag_version
 
 if [ "$current_version" != "$latest_version" ] || [ "$current_tag_version" != "$latest_tag_version" ]; then
     sed -i "s/${current_tag_version}/${latest_tag_version}/g" $manifest_path
